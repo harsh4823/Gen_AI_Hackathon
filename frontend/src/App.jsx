b71e68c777f427c50e3b4d8ff75815550163a668
harsh4823 <harsh924823@gmail.com>
2025-08-29 01:22:56 +0530

    added frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react"
 import { Toaster } from "react-hot-toast"
 import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
+import Navbar from "./Navbar"
 function App() {
   return (
     <div>
       <React.Fragment>
         <Router>
+          <Navbar/>
           <Routes>
             <Route path="/"/>
           </Routes>
